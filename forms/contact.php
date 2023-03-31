81b58dc968f9bbd5f0553aff6996d7bd0c64a712
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2023-03-31 13:39:28 -0400

    mail fixed

--- forms/contact.php
+++ forms/contact.php
@@ -2,16 +2,20 @@
   
   if (isset($_POST['send'])){
     if (!empty($_POST['name']) && !empty($_POST['subject']) && !empty($_POST['message']) && !empty($_POST['email'])) {
-      $name = $_POST['name'];
+      $to = "joseportillo2002.jdpf@gmail.com";
       $subject = $_POST['subject'];
-      $message = $_POST['message'];
-      $email = $_POST['email'];
-      $header = "From: noreply@example.com" . "\r\n";
-      $header .= "Reply-To: noreply@example.com" . "\r\n";
-      $header .= "X-Mailer: PHP/" . phpversion();
-      $mail = @mail($email, $subject, $message, $header);
-      if ($mail) {
+      $message = "Name: " . $_POST['name'] . "<br>";
+      $message .= "Email: " . $_POST['email'] . "<br>";
+      $message .= "Message: " . $_POST['message'] . "<br>";
+
+      $headers = "From: remitente@example.com\r\n";
+      $headers .= "Reply-To: " . $_POST['email'] . "\r\n";
+      $headers .= "Content-type: text/html\r\n";
+
+      if(mail($to, $subject, $message, $headers)) {
         echo "<div class='sent-message'>Your message has been sent. Thank you!</div>";
+      } else {
+        echo "<p>Sorry, there was an error sending your message. Please try again later.</p>";
       }
     }
   }
