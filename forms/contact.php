b7edd6887d22adfeaa6c06fa01f4ba904df726ca
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2023-03-31 13:18:51 -0400

    mail fixed

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,19 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'joseportillo2002.jdpf@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  
+  if (isset($_POST['send'])){
+    if (!empty($_POST['name']) && !empty($_POST['subject']) && !empty($_POST['message']) && !empty($_POST['email'])) {
+      $name = $_POST['name'];
+      $subject = $_POST['subject'];
+      $message = $_POST['message'];
+      $email = $_POST['email'];
+      $header = "From: noreply@example.com" . "\r\n";
+      $header .= "Reply-To: noreply@example.com" . "\r\n";
+      $header .= "X-Mailer: PHP/" . phpversion();
+      $mail = @mail($email, $subject, $message, $header);
+      if ($mail) {
+        echo "<div class='sent-message'>Your message has been sent. Thank you!</div>";
+      }
+    }
   }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
